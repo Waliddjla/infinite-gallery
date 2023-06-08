d0880656b24934af9dc03405700eba5024cb8afe
djellali <walidbibo1997@gmail.com>
2023-06-08 02:25:22 +0100

    add the image diplay and the return buttom

--- src/Components/InfiniteScroll/InfiniteScroll.js
+++ src/Components/InfiniteScroll/InfiniteScroll.js
@@ -7,6 +7,8 @@ export default function InfiniteScroll() {
   const[dataImg, setdataImg] = useState([[], [], []])
   const[pageIndex, setpageIndex] = useState(1)
   const[searchState, setsearchState]= useState('random')
+  const[firstCall, setfirstCall] = useState(true)
+  const [selectedImage, setSelectedImage] = useState(null);
 
   const infinitefetchData = () => {
     fetch(`https://api.unsplash.com/search/collections?page=${pageIndex}&per_page=30&query=${searchState}
@@ -37,10 +39,53 @@ export default function InfiniteScroll() {
         
       }
       setdataImg(newFreshState)
+      setfirstCall(false)
 
     })
   }
   console.log(dataImg)
+
+
+
+  const searchFetchData = () => {
+    fetch(`https://api.unsplash.com/search/collections?page=${pageIndex}&per_page=30&query=${searchState}
+    &client_id=1Pib4UFXj_Zkns-OlxPQfDQ-7fVIrWbuKF8tW1HAAdA`)
+    .then(response => {
+      return response.json()
+    })
+    .then((data) => { 
+      console.log(data);
+      const imgReceived = [];
+      data.results.forEach((resulat) => {
+        resulat.preview_photos.forEach((img) => {
+          imgReceived.push(img.urls.regular)
+
+        })
+      })
+      const newFreshState = [
+        [],
+        [],
+        [],
+      ]
+      let index = 0;
+      for (let i = 0; i < 3; i++) {
+        for(let j = 0; j < 10; j++){
+          newFreshState[i].push(imgReceived[index])
+          index++;
+        }
+        
+      }
+      setdataImg(newFreshState)
+
+    })
+  }
+  useEffect(() => {
+    if(firstCall) return;
+    searchFetchData()
+
+  }, [searchState])
+
+
 
   useEffect(() => {
     infinitefetchData();
@@ -48,6 +93,8 @@ export default function InfiniteScroll() {
  
   const handelSearch = e => {
     e.preventDefault()
+    setsearchState(inpref.current.value)
+    setpageIndex(1)
   }
   const inpref = useRef();
 
@@ -68,32 +115,60 @@ export default function InfiniteScroll() {
       setpageIndex(pageIndex => pageIndex + 1)
     }
   }
+  const handleResetPage = () => {
+  setpageIndex(1);
+  setsearchState('random');
+};
+const handleImageClick = (img) => {
+    setSelectedImage(img);
+  };
+  const handleCloseModal = () => {
+    setSelectedImage(null);
+  };
+
 
   return (
     <div className = "container">
+     {pageIndex > 1 || searchState !== 'random' ? ( // Check if pageIndex is greater than 1
+      <div className="return">
+        <button onClick={handleResetPage}>Return</button>
+      </div>
+     ): null}
     <form onSubmit={handelSearch}>
 
     <label htmlFor="search">Votre Recherche</label>
     <input type="text" id='search' ref={inpref} />
     </form>
+    
     <div className="card-list">
       <div>
         {dataImg[0].map(img => {
-          return <img key={uuidv4()} src={img} alt='image unsplash'/>
+          return <img key={uuidv4()} src={img} alt='image unsplash' onClick={() => handleImageClick(img)}/>
         })}
       </div>
       <div>
       {dataImg[1].map(img => {
-          return <img key={uuidv4()} src={img} alt='image unsplash'  />
+          return <img key={uuidv4()} src={img} alt='image unsplash' onClick={() => handleImageClick(img)}  />
         })}
       </div>
       <div>
       {dataImg[2].map(img => {
-          return <img key={uuidv4()} src={img} alt='image unsplash'/>
+          return <img key={uuidv4()} src={img} alt='image unsplash' onClick={() => handleImageClick(img)}/>
         })}
       </div>
     </div>
+    {selectedImage && (
+        <div className="modal">
+          <div className="modal-content">
+            <span className="close" onClick={handleCloseModal}>
+              &times;
+            </span>
+            <img src={selectedImage} alt="Selected Image" className="modal-image" />
+          </div>
+        </div>
+      )}
+
     
     </div>
-  )
+  );
 }
